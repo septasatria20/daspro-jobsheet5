2a5a8fe853ed286eaffacae930130571eb31cd93
Dwi Septa Satria Agung <144086401+septasatria20@users.noreply.github.com>
2023-09-26 14:58:22 +0700

    Add files via upload

---
+++ PemilihanPercobaan310_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan310_1{
+    public static void main(String[] args) {
+        double angka1, angka2, hasil;
+        char operator;
+        Scanner input10 = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input10.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input10.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = input10.next().charAt(0);
+switch (operator) {
+    case '+':
+        hasil = angka1 + angka2;
+        System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+        case '-':
+        hasil = angka1 - angka2;
+        System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+        break;
+        case '*':
+        hasil = angka1 * angka2;
+        System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+        break;
+        case '/':
+        hasil = angka1 / angka2;
+        System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+        break;
+        }
+    }
+}
